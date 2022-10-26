fa1db31a96dbaec84f98e156439d26fd01e79c9a
Jm Santiago <116279303+sntgjm@users.noreply.github.com>
2022-10-26 16:15:52 +0800

    fivevariables updated

---
+++ fivevariables.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace five_variables_github
+{
+    internal class fivevariables
+    {
+        static void Main(string[] args)
+        {
+            string name = "sntgjm";
+
+            int age = 21;
+
+            double single = 69.0D;
+
+            bool taken = true;
+
+            char rot = 'D';
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(single);
+            Console.WriteLine(taken);
+            Console.WriteLine(rot);
+        }
+    }
+}
